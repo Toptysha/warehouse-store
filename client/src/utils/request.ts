9f23a142b8165d65ac4b19d7a9e81837a0c35950
Vladislav Baranov <toptun113@gmail.com>
2024-11-28 21:28:01 +0300

    +credentianals

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -1,9 +1,9 @@
 export const request = (url: string, method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH', data?: any) => {
-	// return fetch(`http://localhost:3000${url}`, {
 	return fetch(`https://server.warehouse-store.online${url}`, {
 		headers: {
 			'Content-Type': 'application/json',
 		},
+		credentials: 'include',
 		method: method || 'GET',
 		body: data ? JSON.stringify(data) : undefined,
 	}).then((response) => response.json());
--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // app.use(express.static("../client/build"));
 
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
